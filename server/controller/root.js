af6b3011cdc9b0b191fe78f7f877f97825025e5e
Surya <febriantosurya@ce.student.pens.ac.id>
2023-02-12 15:59:06 +0700

    feat: add middleware jwt to generate and verify token

--- server/controller/root.js
+++ server/controller/root.js
@@ -1,4 +1,6 @@
 const rootRepos = require('../repositories/root')
+const adminRepos = require('../repositories/admin')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 exports.login = async (req, res) => {
@@ -10,11 +12,12 @@ exports.login = async (req, res) => {
     }
     const password = req.body.password
     if (password === root.password) {
-      return res.json({ message: "login success" })
+      const token = jwt.sign({
+        data: root
+      }, `${process.env.ROOT_SECRET_KEY}`, { expiresIn: '1h' })
+      return res.status(200).json({ message: "success", token: token })
     }
-    else {
-      return res.json({ message: "password incorrect"})
-    }
+    return res.json({ message: "password incorrect"})
   } catch (error) {
     return res.status(400).json({ message: `failed ${error.message}` })
   }
@@ -24,6 +27,26 @@ exports.getAllAdmins = async (req, res) => {
   try {
     const admins = await rootRepos.getAdmins()
     return res.status(200).json({ message: "success", data: admins });
+  } catch (error) {
+    return res.status(400).json({ message: `failed ${error.message}` })
+  }
+}
+
+exports.addAdmin = async (req, res) => {
+  try {
+    const data = {
+      username: req.body.username,
+      password: req.body.password,
+      name: req.body.name,
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    }
+    const username = await adminRepos.getAdmin({ username: data.username })
+    if (username !== null) {
+      return res.status(400).json({ message: "failed username has been used" })
+    }
+    await rootRepos.addAdmin(data)
+    return res.status(200).json({ message: "new admin added" })
   } catch (error) {
     return res.status(400).json({ message: `failed ${error.message}` })
   }
