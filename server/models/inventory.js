4fe09a5baa3776f5d41f3fef50c1cf5260f9ecfc
Surya <febriantosurya@ce.student.pens.ac.id>
2023-02-24 15:09:53 +0700

    feat: add inventory model and repository

---
+++ server/models/inventory.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../configs/database');
+
+const Inventory = sequelize.define('Inventory', {
+  idAdmin: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  merk: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  unit: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  condition: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  note: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  source: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  createdAt: {
+    allowNull: true,
+    type: DataTypes.DATE
+  },
+  updatedAt: {
+    allowNull: true,
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  tableName: "inventories",
+  modelName: "Inventory"
+});
+
+module.exports = Inventory;
