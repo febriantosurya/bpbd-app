60c6fa27c75d03e74dc4b08c3fdb2204e4ebde12
febriantosurya <febriantosurya5@gmail.com>
2023-02-24 16:52:53 +0700

    fix: change import variable name

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const userController = require('../controller/user')
-const rootMiddleware = require('../middlewares/authorizationRoot')
+const middlware = require('../middlewares/authorization')
 
 const router = express.Router()
 
-router.post('/auth/root-login', userController.login)
-router.post('/root-add-admin', rootMiddleware.checkAuthorization, userController.addAdmin)
-router.get('/root-mainpage', rootMiddleware.checkAuthorization, userController.showAdmins)
-router.delete('/root-remove-admin', rootMiddleware.checkAuthorization, userController.deleteAdmin)
-router.put('/root-update-admin', rootMiddleware.checkAuthorization, userController.updateAdmin)
+router.post('/auth/login', userController.login)
+router.post('/root-add-admin', middlware.checkRootAuthorization, userController.addAdmin)
+router.get('/root-mainpage', middlware.checkRootAuthorization, userController.showAdmins)
+router.delete('/root-remove-admin', middlware.checkRootAuthorization, userController.deleteAdmin)
+router.put('/root-update-admin', middlware.checkRootAuthorization, userController.updateAdmin)
 
 module.exports = router
