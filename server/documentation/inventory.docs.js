734289905af5d81a93ea633db64b7f3468f9dc4d
febriantosurya <febriantosurya5@gmail.com>
2023-02-25 20:38:57 +0700

    feat: add documentation for update inventory

--- server/documentation/inventory.docs.js
+++ server/documentation/inventory.docs.js
@@ -125,11 +125,118 @@ const addInventory = {
   }
 }
 
+const updateInventory = {
+  tags: ["Admin"],
+  security: [{ bearerAuth: [] }],
+  summary: "Update inventory",
+  description: "This API is used to update inventory by using ID",
+  requestBody: {
+    content: {
+      "application/json": {
+        schema: {
+          type: "object",
+          properties: {
+            id: {
+              type: "int",
+              nullable: false,
+              description: "id of inventory to update",
+              example: 1
+            },
+            name: {
+              type: "string",
+              nullable: false,
+              description: "name of inventory to update",
+              example: "perahu"
+            },
+            brand: {
+              type: "string",
+              nullable: true,
+              description: "brand of inventory to update",
+              example: "dolphin"
+            },
+            quantity: {
+              type: "int",
+              nullable: false,
+              description: "quantity of inventory to update",
+              example: 2
+            },
+            unit: {
+              type: "string",
+              nullable: true,
+              description: "unit kind of inventory to update",
+              example: "pasang"
+            },
+            condition: {
+              type: "string",
+              nullable: true,
+              description: "condition of inventory to update",
+              example: "baik"
+            },
+            note: {
+              type: "string",
+              nullable: true,
+              description: "update note for inventory",
+              example: "perahu sedang berada di sarangan"
+            },
+            source: {
+              type: "string",
+              nullable: true,
+              description: "update source of inventory",
+              example: "apbd"
+            },
+            year: {
+              type: "int",
+              nullable: false,
+              description: "year of inventory to update",
+              example: 2022
+            }
+          }
+        }
+      }
+    }
+  },
+  responses: {
+    200: {
+      description: "success",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            example: {
+              "id": 1,
+              "idAdmin": 2,
+              "name": "perahu",
+              "brand": "dolphin",
+              "quantity": 3,
+              "unit": "pasang",
+              "condition": "baik",
+              "note": "perahu sedang berada di sarangan",
+              "source": "apbd",
+              "year": 2022,
+              "createdAt": "2023-02-25T08:44:07.171Z",
+              "updatedAt": "2023-02-25T08:44:07.172Z"
+            }
+          }
+        }
+      }
+    },
+    400: {
+      description: "bad request"
+    },
+    401: {
+      description: "not found"
+    }
+  }
+}
+
 exports.invenDoc = {
   "/api/v1/inventory/{year}": {
     get: getInventoriesByYear
   },
   "/api/v1/inventory/add": {
     post: addInventory
+  },
+  "/api/v1/inventory/update": {
+    put: updateInventory
   }
 }
