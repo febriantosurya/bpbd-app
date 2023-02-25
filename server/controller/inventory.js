6853639963d0ad4932c0a0be1e376e773d831e70
febriantosurya <febriantosurya5@gmail.com>
2023-02-25 14:47:08 +0700

    fix: change body to params for year

--- server/controller/inventory.js
+++ server/controller/inventory.js
@@ -2,9 +2,9 @@ const inventoryRepo = require('../repositories/inventory');
 
 exports.getInventoryByYear = async (req, res) => {
   try {
-    const year = { year: req.body.year }
+    const year = { year: req.params.year }
     inventories = await inventoryRepo.getInventories(year);
-    return res.status(400).json({ message: "success", data: inventories })
+    return res.status(200).json({ message: "success", data: inventories })
   } catch (error) {
     return res.status(400).json({ message: `failed ${error.message}` })
   }
