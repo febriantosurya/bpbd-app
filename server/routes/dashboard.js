6178cce66bac60c1273eaed4cc420bcff4c4067c
febriantosurya <febriantosurya5@gmail.com>
2023-05-29 16:19:12 +0700

    fix: separate dashboard user and admin

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dashboardController = require('../controller/dashboard');
+const { checkAdminAuthorization, checkUserAuthorization } = require('../middlewares/authorization');
 
 const router = express.Router();
 
-router.get('/', dashboardController.showDashboard);
+router.get('/a', checkAdminAuthorization, dashboardController.showDashboard);
+router.get('/u', checkUserAuthorization, dashboardController.showDashboard);
 
 module.exports = router;
