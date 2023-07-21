b7c6897d143bd55586d0b20a23800a9f29860132
febriantosurya <febriantosurya5@gmail.com>
2023-07-21 23:15:43 +0700

    feat: searching on archive active

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -4,5 +4,6 @@ import BencanaGO from './image/bencanaGO.png'
 import GOopacity from './image/GOopacity30.png'
 import SideBarIcon from './image/sidebar.png'
 import BurgerMenu from './image/burgerMenu.svg'
+import SearchIcon from './image/search.png'
 
-export { ImageLogo, Logout, BencanaGO, GOopacity, SideBarIcon, BurgerMenu };
+export { ImageLogo, Logout, BencanaGO, GOopacity, SideBarIcon, BurgerMenu, SearchIcon };
