71db8d5fcbfe304a6d2af04555f949b1d2e6ca40
febri <febriantosurya>
2023-07-30 16:16:38 +0700

    fix dashboard api in server and client

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const dashboardController = require('../controller/dashboard');
-const { checkAdminAuthorization, checkUserAuthorization } = require('../middlewares/authorization');
+const { checkAdminAuthorization, checkUserAuthorization, checkAdminAndUser } = require('../middlewares/authorization');
 
 const router = express.Router();
 
-router.get('/a', checkAdminAuthorization, dashboardController.showDashboard);
-router.get('/u', checkUserAuthorization, dashboardController.showDashboard);
+router.get('/', checkAdminAndUser, dashboardController.showDashboard);
 
 module.exports = router;
