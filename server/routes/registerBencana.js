b665e4c2cb09728e9a4f0ec03d657b3206bd2af9
febriantosurya <febriantosurya5@gmail.com>
2023-07-31 22:19:39 +0700

    fix: change authorization and deleting unnecessary codes

--- server/routes/registerBencana.js
+++ server/routes/registerBencana.js
@@ -4,9 +4,6 @@ const middleware = require('../middlewares/authorization');
 
 const router = express.Router();
 
-router.get('/:month/:year', middleware.checkAdminAuthorization, regBencanaController.showAllRegBencanaByMonth);
-router.post('/add-reg-bencana', middleware.checkAdminAuthorization, regBencanaController.addRegBencana);
-router.put('/edit-reg-bencana', middleware.checkAdminAuthorization, regBencanaController.editRegBencana);
-router.delete('/del-reg-bencana', middleware.checkAdminAuthorization, regBencanaController.deleteRegBencana);
+router.get('/:month/:year', middleware.checkAdminAndUser, regBencanaController.showAllRegBencanaByMonth);
 
 module.exports = router;
